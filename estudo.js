d585bf390d24d5a081f58803ae130f687e1ca40f
CalebeKun <105517982+CalebeKun@users.noreply.github.com>
2022-10-19 08:59:47 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,42 +1,10 @@
-let tabuada = 4;
-
-function escreva(){
-    document.write("<h1>Tabuada do " + tabuada + "</h1>");
-    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10)+"<br>");
-}
 
 
-function tabuada(){
-    for(let i=1; i < 5; i++){
-        document.write("tabuada do " + i + "<br>");
-        for(let i=0; i < 5; i++){
-        document.write(i + " x " + j + " = " + (tabuada*i)+"<br>");
-    }
-    document.write("<br>")
-  }
-}
+function sn (){
+    let n1 = document.getElementById("t1"). Value;
+    let n2 = document.getElementById("t2"). Value;
+    let n3 = document.getElementById("t3"). Value;
+    let r = Number("n1") + Number("n2") + Number("n3");
+    document.getElementById("rult"). innerHTML = "soma: " + r;
 
-function quadrado(){
-    for(let i=1; i < 5; i++){
-        document.write("O quadrado de " + i + " é " + (i*i) + "<br>")
-    }
-}
-function calcula(){
-    let val = document.getElementById("valor").Value;
-    let j = document.getElementById("juros").Value;
-    let t = document.getElementById("meses").Value;
-
-    let res = val * (1+(j/100));
-
-    document.write("Resultado: "+res);
-    
 }
